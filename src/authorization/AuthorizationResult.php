30bd0c504e782142980935bda085d976b2d0e917
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-07-24 11:09:39 +0300

    * bugfix

--- src/authorization/AuthorizationResult.php
+++ src/authorization/AuthorizationResult.php
@@ -3,10 +3,10 @@
  * Enum that contains all available authorization statuses.
  */
 interface AuthorizationResultStatus {
-	const OK = 5;
-	const UNAUTHORIZED = 6;
-	const FORBIDDEN = 7;
-	const NOT_FOUND = 8;
+	const OK = 6;
+	const UNAUTHORIZED = 7;
+	const FORBIDDEN = 8;
+	const NOT_FOUND = 9;
 }
 
 /**
