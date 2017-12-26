9a4c96051184d0c695a9e1a3d2f0e37aaab8d695
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-12-26 14:12:10 +0200

    refactored OAuth2Authentication

--- src/authentication/OAuth2UserInformation.php
+++ src/authentication/OAuth2UserInformation.php
@@ -1,49 +1,26 @@
 <?php
 /**
- * Encapsulates information about remote logged in user on OAuth2 provider.
+ * Encapsulates abstract information about remote logged in user on OAuth2 provider.
  */
-class OAuth2UserInformation {
-	private $id;
-	private $name;
-	private $email;
-	
-	/**
-	 * Creates an object.
-	 * 
-	 * @param integer $id Remote user id.
-	 * @param string $name Remote user name.
-	 * @param string $email Remote user email.
-	 */
-	public function __construct($id, $name, $email) {
-		$this->id = $id;
-		$this->name = $name;
-		$this->email = $email;
-	}
-	
+interface OAuth2UserInformation {	
 	/**
 	 * Gets remote user id.
 	 * 
 	 * @return integer
 	 */
-	public function getId() {
-		return $this->id;
-	}
+	function getId();
 	
 	/**
 	 * Gets remote user name.
 	 * 
 	 * @return string
 	 */
-	public function getName() {
-		return $this->name;
-	}
+	function getName();
 	
 	/**
 	 * Gets remote user email.
 	 * 
 	 * @return string
 	 */
-	public function getEmail() {
-		return $this->email;
-	}
+	function getEmail();
 }
